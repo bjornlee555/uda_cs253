c46abb6cf2ee97986bc030b1eb88392af91c59ed
bjornlee555 <bjornlee.inc@gmail.com>
2016-03-03 01:38:52 +0800

    Modified to include redirecting to Welcome page and works for valid signup data

--- problem_set2.2/user_signup.py
+++ problem_set2.2/user_signup.py
@@ -1,5 +1,6 @@
 import webapp2
 import cgi
+import re
 
 form = """
 <!DOCTYPE html>
@@ -9,6 +10,7 @@ form = """
 		<title>User Signup</title>
 		<style type="text/css">
 			.label {text-align: right}
+			.error {color:red}
 		</style>
 	</head>
 
@@ -21,7 +23,11 @@ form = """
 						Username
 					</td>
 					<td>
-						<input type="text" name="username">
+						<input type="text" name="username" value="%(username)s" required>
+					</td>
+					
+					<td class="error">
+						<div >%(error_1)s</div>
 					</td>
 				</tr>
 				<tr>
@@ -29,7 +35,10 @@ form = """
 						Password
 					</td>
 					<td>
-						<input type="text" name="password">
+						<input type="text" name="password" value="%(password)s" required>
+					</td>
+					<td class="error">
+						<div >%(error_2)s</div>
 					</td>
 				</tr>
 				<tr>
@@ -37,7 +46,10 @@ form = """
 						Verify Password
 					</td>
 					<td>
-						<input type="text" name="verify">
+						<input type="text" name="verify" value="%(verify)s" required>
+					</td>
+					<td class="error">
+						<div >%(error_3)s</div>
 					</td>
 				</tr>
 
@@ -46,7 +58,10 @@ form = """
 						Email (optional)
 					</td>
 					<td>
-						<input type="text" name="email">
+						<input type="text" name="email" value="%(email)s">
+					</td>
+					<td class="error">
+						<div >%(error_4)s</div>
 					</td>
 				</tr>
 							
@@ -61,11 +76,58 @@ form = """
 
 """
 
+def escape_html(s):
+	return cgi.escape(s, quote = True)
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+
+def valid_username(username):
+	return USER_RE.match(username)
+
+PW_RE = re.compile("^.{3,20}$")
+
+def valid_password(password):
+	return PW_RE.match(password)
+
+EMAIL_RE = re.compile("^[\S]+@[\S]+\.[\S]+$")
+
+def valid_email(email):
+	return EMAIL_RE.match(email)
+
 class MainPage(webapp2.RequestHandler):
 
+	def write_form(self, error_1="", error_2="", error_3="", error_4="", 
+					username="", password="",verify="",email=""):
+		self.response.out.write(form % {"error_1": error_1, "error_2": error_2, 
+										"error_3": error_3, "error_4": error_4, 
+										"username":escape_html(username), 
+										"password":escape_html(password), 
+										"verify":escape_html(verify), 
+										"email":escape_html(email)})
+
 	def get(self):
-		self.response.write(form)
+		self.write_form()
+		#self.response.write(form)
+
+	def post(self):
+		a_username = self.request.get('username')
+		a_password = self.request.get('password')
+		a_verify = self.request.get('verify')
+		a_email = self.request.get('email')
+
+		username = valid_username(a_username)
+		password = valid_password(a_password)
+		verify=valid_password(a_verify)
+		email = valid_email(a_email)
+
+		if username and password and a_password==a_verify:
+			self.redirect("/welcome")
+
+class WelcomeHandler(webapp2.RequestHandler):
+	def get(self):
+		self.response.out.write("Welcome, ")
+
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)], debug=True)
+    ('/', MainPage), ('/welcome', WelcomeHandler)], debug=True)
