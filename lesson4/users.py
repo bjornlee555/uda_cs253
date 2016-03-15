e7336290224a0345ea3fbc7ac97766f103a499f6
bjornlee555 <bjornlee.inc@gmail.com>
2016-03-15 17:16:44 +0800

    Updated app to fix error of extra spaces in string format operators

--- lesson4/users.py
+++ lesson4/users.py
@@ -2,6 +2,9 @@ import os
 import webapp2
 import jinja2
 import hashlib
+import hmac
+
+SECRET = 'imsosecret'
 
 from google.appengine.ext import db
 
@@ -9,22 +12,24 @@ template_dir = os.path.join(os.path.dirname(__file__),'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 								autoescape=True)
 
-cookie_separator = '|'
+
 def hash_str(s):
-	return hashlib.md5(s).hexdigest()
+	return hmac.new(SECRET,s).hexdigest()
 
 def make_secure_val(s):
-	return "%s %s %s" % (s, cookie_separator, hash_str(s))
+	return '%s|%s' % (s,  hash_str(s))
 
 def check_secure_val(h):
-	val = h.split(cookie_separator)[0]
+	val = h.split('|')[0]
 	if h == make_secure_val(val):
 		return val
+	else:
+		return None
 
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
-		self.response.write(*a, **kw)
+		self.response.out.write(*a, **kw)
 
 	def render_str(self,template, **params):
 		t = jinja_env.get_template(template)
@@ -34,6 +39,7 @@ class Handler(webapp2.RequestHandler):
 		self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
+
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
 		visits = 0
@@ -45,16 +51,16 @@ class MainPage(Handler):
 				visits = int(cookie_val)
 
 		visits += 1
-
 		new_cookie_val = make_secure_val(str(visits))
 
-		self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
+		self.response.headers.add_header('Set-Cookie', 'visits=%s' %new_cookie_val)
 
-		if visits > 10010:
-			self.write('You are the best ever!')
+		if visits > 100:
+			self.write("You are the best ever!")
 		else:
-			self.write("You've been here %s times!" % str(visits))
+			self.write("You've been here %s times" % str(visits))
 
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+
 
